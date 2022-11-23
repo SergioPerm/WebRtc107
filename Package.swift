b53be89d6ae73f1e3c3681f74eb5c92ab13a54d1
Sergey Lepinin <sergey.lepinin@interactivestandard.com>
2022-11-23 14:47:39 +0700

    Add M107 SPM configuration

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/SergioPerm/WebRtc107/releases/download/107.0.0/WebRTC-M107.xcframework.zip",
+            checksum: "74990104951c584b14ef2ee588e40b874a4bc1ad734746b83b24ad3973c7cb4e"
+        ),
+    ]
+)
